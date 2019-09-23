6e455bb2010470e2097fee53846a1d6282990803
Ben Lye <ben@lye.co.nz>
2019-09-23 17:42:54 -0400

    Update buildmulti.sh

--- buildmulti.sh
+++ buildmulti.sh
@@ -7,57 +7,87 @@ function showusage
 {
     printf "Usage:\n"
     printf "  docker run --rm -it -v \"[path to source]:/multi\" -e \"BOARD=[board]\" benlye/multi4in1-build\n\n"
-
     printf "Where:\n"
     printf "  [path to source] is the path to the folder where the Multiprotocol source was cloned or unzipped\n"
-    printf "  [board] is the FQBN of the desired board\n\n"
-    printf "If no board is speciifed then 'multi4in1:STM32F1:multistm32f103c:debug_option=none' will be used.\n\n"
-    printf "Example:\n"
-    printf "  docker run --rm -it -v \"C:\\Users\\benlye\\Downloads\\DIY-Multiprotocol-TX-Module:/multi\" -e \"BOARD=multi4in1:STM32F1:multistm32f103c:debug_option=none\" benlye/multi4in1-build\n\n"
+    printf "  [board] case-sensitive is the FQBN or alias of the desired board\n\n"
     printf "Valid boards:\n"
-    printf "  AVR Boards\n"
-    printf "    multi4in1:avr:multiatmega328p:bootloader=none\n"
-    printf "    multi4in1:avr:multiatmega328p:bootloader=optiboot\n\n"
-    printf "  STM32 Boards\n"
-    printf "    multi4in1:STM32F1:multistm32f103c:debug_option=none\n"
-    printf "    multi4in1:STM32F1:multistm32f103c:debug_option=native\n"
-    printf "    multi4in1:STM32F1:multistm32f103c:debug_option=ftdi\n\n"
-    printf "  OrangeRX Board\n"
-    printf "    multi4in1:avr:multixmega32d4\n\n"
+    printf "  Type     | Alias           | FQBN\n"
+    printf "  ---------|-----------------|------------------------------------------------------\n"
+    printf "  AVR      | avr             | multi4in1:avr:multiatmega328p:bootloader=none\n"
+    printf "  AVR      | avr-boot        | multi4in1:avr:multiatmega328p:bootloader=optiboot\n"
+    printf "  STM32    | stm32           | multi4in1:STM32F1:multistm32f103c:debug_option=none\n"
+    printf "  STM32    | stm32-usbdebug  | multi4in1:STM32F1:multistm32f103c:debug_option=native\n"
+    printf "  STM32    | stm32-ftdidebug | multi4in1:STM32F1:multistm32f103c:debug_option=ftdi\n"
+    printf "  OrangeRX | orx             | multi4in1:avr:multixmega32d4\n\n"
+    printf "Examples:\n"
+    printf '  docker run --rm -it -v \"C:\\Users\\benlye\\Downloads\\DIY-Multiprotocol-TX-Module:/multi\" -e \"BOARD=stm32\" benlye/multi4in1-build\n\n'
+    printf '  docker run --rm -it -v \"C:\\Users\\benlye\\Downloads\\DIY-Multiprotocol-TX-Module:/multi\" -e \"BOARD=multi4in1:STM32F1:multistm32f103c:debug_option=native\" benlye/multi4in1-build\n\n'
+    printf "'Devel' boards can be specified by appending '-devel' to the alias (e.g. 'stm32-devel') or replacing 'multi' with 'multi-devel' in the FQBN.\n\n"
 }
 
 # Error if the Multi source isn't found
 if [ ! -f /multi/Multiprotocol/Multiprotocol.ino ]; then
-    printf "ERROR: Multiprotocol source not found.\n\n"
+    printf "\e[91mERROR: Multiprotocol source not found.\e[0m\n\n"
     showusage
     exit 1
 fi
 
-# Default to the STM32 if a board wasn't specified
-if [ "x" == "x$BOARD" ]; then
-    printf "No board specified, defaulting to 'multi4in1:STM32F1:multistm32f103c:debug_option=none'.\n\n"
-    BOARD="multi4in1-devel:STM32F1:multistm32f103c"
-fi
-
-# Remap boards specified without options to the default option
-if [ "$BOARD" == "multi4in1:avr:multiatmega328p" ] || [ "$BOARD" == "multi4in1-devel:avr:multiatmega328p" ]; then
-    BOARD="$BOARD:bootloader=none"
-fi
-if [ "$BOARD" == "multi4in1:STM32F1:multistm32f103c" ] || [ "$BOARD" == "multi4in1-devel:STM32F1:multistm32f103c" ]; then
-    BOARD="$BOARD:debug_option=none"
-fi
-
-# Error if the board is not recognized
-if [ "$BOARD" != "multi4in1:avr:multiatmega328p:bootloader=none" ] && [ "$BOARD" != "multi4in1:avr:multiatmega328p:bootloader=optiboot" ] \
-    && [ "$BOARD" != "multi4in1-devel:avr:multiatmega328p:bootloader=none" ] && [ "$BOARD" != "multi4in1-devel:avr:multiatmega328p:bootloader=optiboot" ] \
-    && [ "$BOARD" != "multi4in1:STM32F1:multistm32f103c:debug_option=none" ] && [ "$BOARD" != "multi4in1:STM32F1:multistm32f103c:debug_option=native" ] && [ "$BOARD" != "multi4in1:STM32F1:multistm32f103c:debug_option=ftdi" ] \
-    && [ "$BOARD" != "multi4in1-devel:STM32F1:multistm32f103c:debug_option=none" ] && [ "$BOARD" != "multi4in1-devel:STM32F1:multistm32f103c:debug_option=native" ] && [ "$BOARD" != "multi4in1-devel:STM32F1:multistm32f103c:debug_option=ftdi" ] \
-    && [ "$BOARD" != "multi4in1:avr:multixmega32d4" ] \
-    && [ "$BOARD" != "multi4in1-devel:avr:multixmega32d4" ]; then
-    printf "ERROR: Unknown board '$BOARD' specified.\n\n"
-    showusage
-    exit 1
-fi
+# Remap board aliases and abbreviations to board FQBNs
+case "$BOARD" in
+    "avr")
+        ;&
+    "multi4in1:avr:multiatmega328p")
+        BOARD="multi4in1:avr:multiatmega328p:bootloader=none"
+        ;;
+    "avr-devel")
+        ;&
+    "multi4in1-devel:avr:multiatmega328p")
+        BOARD="multi4in1-devel:avr:multiatmega328p:bootloader=none"
+        ;;
+    "avr-boot")
+        BOARD="multi4in1:avr:multiatmega328p:bootloader=optiboot"
+        ;;
+    "avr-boot-devel")
+        BOARD="multi4in1-devel:avr:multiatmega328p:bootloader=optiboot"
+        ;;
+    "stm32")
+        ;&
+    "multi4in1:STM32F1:multistm32f103c")
+        BOARD="multi4in1:STM32F1:multistm32f103c:debug_option=none"
+        ;;
+    "stm32-devel")
+        ;&
+    "multi4in1-devel:STM32F1:multistm32f103c")
+        BOARD="multi4in1-devel:STM32F1:multistm32f103c:debug_option=none"
+        ;;
+    "stm32-usbdebug")
+        BOARD="multi4in1:STM32F1:multistm32f103c:debug_option=native"
+        ;;
+    "stm32-usbdebug-devel")
+        BOARD="multi4in1-devel:STM32F1:multistm32f103c:debug_option=native"
+        ;;
+    "stm32-ftdidebug")
+        BOARD="multi4in1:STM32F1:multistm32f103c:debug_option=ftdi"
+        ;;
+    "stm32-ftdidebug-devel")
+        BOARD="multi4in1-devel:STM32F1:multistm32f103c:debug_option=ftdi"
+        ;;
+    "orx")
+        BOARD="multi4in1:avr:multixmega32d4"
+        ;;
+    "orx-devel")
+        BOARD="multi4in1-devel:avr:multixmega32d4"
+        ;;
+    "")
+        printf "\e[91mERROR: No board specified.\e[0m\n\n"
+        showusage
+        exit 1
+        ;;
+    *)
+        printf "\e[91mERROR: Unknown board '$BOARD' specified.\e[0m\n\n"
+        showusage
+        exit 1
+esac
 
 # Check for an update for the specified board
 printf "Checking for Multi4in1 board package updates ...\n"
@@ -97,20 +127,21 @@ REVISION_VERSION=$(grep "VERSION_REVISION" "/multi/Multiprotocol/Multiprotocol.h
 PATCH_VERSION=$(grep "VERSION_PATCH" "/multi/Multiprotocol/Multiprotocol.h" | awk -v N=3 '{gsub(/\r/,""); print $N}')
 MULTI_VERSION=$MAJOR_VERSION.$MINOR_VERSION.$REVISION_VERSION.$PATCH_VERSION
 
-# Output some information
-printf "\nBuilding firmware version v$MULTI_VERSION ...\n\n"
-
 # Compile the firmware
+printf "\nBuilding firmware version v$MULTI_VERSION ...\n\n"
 printf "arduino-cli compile -b $BOARD /tmp/build/Multiprotocol/Multiprotocol.ino --build-path /build/output\n\n"
 arduino-cli compile -b $BOARD /tmp/build/Multiprotocol/Multiprotocol.ino --build-path /build/output
 
+# Show the file path if we succeeded, an error mesage if not
 if [ $? -eq 0 ]; then
     # Get the name of the compiled firmware file
     FWBINFILE=(`find /build/output/multi-*.bin -printf "%f\n"`)
     # Copy the versioned firmware file to the source folder
     cp /build/output/$FWBINFILE /multi/Multiprotocol/
 
-    printf "\nCompiled Multiprotocol firmware saved as '/multi/Multiprotocol/$FWBINFILE'.\n"
+    printf "\n\e[92mCompiled Multiprotocol firmware saved as '/multi/Multiprotocol/$FWBINFILE'.\e[0m\n"
+else
+    printf "\n\e[91mBuild failed.\e[0m\n\n"
 fi
 
 printf "\n"
